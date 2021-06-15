af42f4675b8ffa93d99bc189bf6ddb5dfeb089ff
Rustem-Erdem <rustemerdem129@gmail.com>
2021-06-14 19:35:13 -0500

    messages

---
+++ src/Demo3.java
@@ -0,0 +1,9 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int num = 10;
+        int num1 =20;
+        num1 =num;
+        num = 30;
+        System.out.println(num1);
+    }
+}
